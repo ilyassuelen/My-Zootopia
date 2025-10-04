6a65d72fe258fbe42c51d0517f57ecfe6c5b9489
Ilyas Sülen <ilyassulen@Mac.local>
2025-10-04 12:51:26 +0200

    generate card item for every animal

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -14,22 +14,28 @@ animals_data = load_data('animals_data.json')
 output = ""
 
 for animal in animals_data:
+    # append information to each string
     name = animal.get("name")
     locations = animal.get("locations", [])
     characteristics = animal.get("characteristics", {})
     diet = characteristics.get("diet")
     type_ = characteristics.get("type")
 
-    if name:
-        output += f"Name: {name}\n"
-    if diet:
-        output += f"Diet: {diet}\n"
-    if locations:
-        output += f"Location: {locations[0]}\n"
-    if type_:
-        output += f"Type: {type_}\n"
+    # Begin <li>
+    output += '<li class="cards__item">\n'
 
-    output += "\n"
+    if name:
+        output += f"Name: {name}<br/>\n"
+    if diet:
+        output += f"Diet: {diet}<br/>\n"
+    if locations:
+        output += f"Location: {locations[0]}<br/>\n"
+    if type_:
+        output += f"Type: {type_}<br/>\n"
+
+    # End </li>
+    output += '</li>\n'
+
 
 # Read Template
 with open("animals_template.html", "r", encoding="utf-8") as fileobj:
@@ -42,4 +48,4 @@ html_output = template.replace("__REPLACE_ANIMALS_INFO__", output)
 with open("animals.html", "w", encoding="utf-8") as fileobj:
     fileobj.write(html_output)
 
-print("Created 'animals.html'!")
+print("Created 'animals.html' with HTML serialization!")
